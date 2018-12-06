f0c6ac1e62cbec43cc814c70ed23074915b30716
Kris Krej <kris@krej.net>
2018-12-06 20:42:44 +0100

    some code

---
+++ Assets/Code/Conductor.cs
@@ -0,0 +1,20 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conductor : MonoBehaviour {
+	
+	[SerializeField] List<AudioSource> instruments;
+	
+	
+	void Update () {
+		foreach (var instrument in instruments)
+			instrument.volume = CalculateVolume(listener.transform.position, instrument.transform.position);
+	}
+
+	float CalculateVolume(Vector3 instrument, Vector3 listener) {
+		var distance = Vector3.Distance(instrument, listener);
+		return Mathf.Clamp01(distance/)
+	}
+}
+*/
---
+++ Assets/Code/Instrument.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrument : MonoBehaviour {
+	[SerializeField] Listener listener;
+	float distanceToConductor;
+	
+	void Start () {
+		listener = FindObjectOfType<Listener>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//distanceToConductor = 
+	}
+}
