0ab855e9f339af4a3290a1fc903927af098e8dd0
Student <student@microsoft.wsei.edu.pl>
2018-12-07 10:15:53 +0100

    nicer scene

--- Assets/Code/Instrument.cs
+++ Assets/Code/Instrument.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Instrument : MonoBehaviour {
 	[SerializeField] Listener listener;
 	float distanceToConductor;
+	float MAX_DISTANCE = 20f;
+	public float distance01;
 	
 	void Start () {
 		listener = FindObjectOfType<Listener>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		//distanceToConductor = 
+		distanceToConductor = Vector3.Distance(transform.position, listener.transform.position);
+		distance01 = Mathf.Clamp01 (distanceToConductor / MAX_DISTANCE);
 	}
 }
