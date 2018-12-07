b71657302e03a10610ee87d74249979a25aa561d
Student <student@microsoft.wsei.edu.pl>
2018-12-07 10:30:22 +0100

    it should work now!

---
+++ Assets/PlayEverythingAtFullMinute.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEverythingAtFullMinute : MonoBehaviour {
+	
+
+	IEnumerator Start () {
+		var sources = FindObjectsOfType<AudioSource> ();
+		while(System.DateTime.Now.Second > 1){
+			Debug.Log (System.DateTime.Now.Second);
+			yield return null;
+		}
+		foreach (var src in sources)
+			src.Play ();
+	}
+
+}
